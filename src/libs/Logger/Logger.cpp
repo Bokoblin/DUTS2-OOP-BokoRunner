df10cf761628646916692ab8c115476d3560075a
Bokoblin <jolivet.arthur@gmail.com>
2018-01-01 21:51:50 +0100

    Logger : h / cpp separation + info method

---
+++ src/libs/Logger/Logger.cpp
@@ -0,0 +1,45 @@
+#include "Logger.h"
+
+namespace Bokoblin
+{
+namespace SimpleLogger
+{
+
+/**
+ * Prints an info message on the console
+ * @param message the message to print along info tag
+ *
+ * @author Arthur
+ * @date 01/01/18
+ */
+void Logger::printInfoOnConsole(const std::string &message)
+{
+    std::cout << "[INFO] " << message << std::endl;
+}
+
+/**
+ * Prints a warning message on the console
+ * @param message the message to print along warning tag
+ *
+ * @author Arthur
+ * @date 28/12/17
+ */
+void Logger::printWarningOnConsole(const std::string &message)
+{
+    std::cout << "[WARNING] " << message << std::endl;
+}
+
+/**
+ * Prints an error message on the console
+ * @param message the message to print along error tag
+ *
+ * @author Arthur
+ * @date 28/12/17
+ */
+void Logger::printErrorOnConsole(const std::string &message)
+{
+    std::cerr << "[ERROR] " << message << std::endl;
+}
+
+} //namespace SimpleLogger
+} //namespace Bokoblin
--- src/libs/Logger/Logger.h
+++ src/libs/Logger/Logger.h
@@ -1,4 +1,4 @@
-/* Copyright 2017 Jolivet Arthur
+/* Copyright 2017-2018 Jolivet Arthur
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,26 +24,19 @@ namespace SimpleLogger
 {
 
 /**
- * TODO : Temporary \n
  * The Logger class currently implements a temporary logger for stdout and stderr. \n
  * Moving forward to Boko Runner 2.0.x release, it should implement
  * a more complexe logger than just writing in program console
  *
  * @author Arthur
- * @date 28/12/17
+ * @date 28/12/17 - 01/01/18
  */
 class Logger
 {
 public:
-    static void printWarningOnConsole(const std::string &message)
-    {
-        std::cout << "[WARNING] " << message << std::endl;
-    }
-
-    static void printErrorOnConsole(const std::string &message)
-    {
-        std::cerr << "[ERROR] " << message << std::endl;
-    }
+    static void printInfoOnConsole(const std::string &message);
+    static void printWarningOnConsole(const std::string &message);
+    static void printErrorOnConsole(const std::string &message);
 };
 
 } //namespace SimpleLogger

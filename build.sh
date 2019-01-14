d55949a12cd5e0ab93b1540787e85321d1c69420
Arthur Jolivet <jolivet.arthur@gmail.com>
2019-01-14 15:52:11 +0100

    Fixed Use { ..; } instead of (..) to avoid subshell overhead

--- build.sh
+++ build.sh
@@ -4,23 +4,24 @@ mkdir -p build
 cd build
 rm -rf ./*
 
-if [ $# -eq 1 ] && ([ $1 == "-h" ] || [ $2 == "--help" ])
+if [[ $# -eq 1 ]] && { [[ $1 == "-h" ]] || [[ $2 == "--help" ]]; }
+
 then
     echo "Usage :"
     echo "$0"
     echo "$0 -c <clang|gnu>"
-elif [ $# -eq 2 ] && [ $1 == "-c" ]
+elif [[ $# -eq 2 ]] && [[ $1 == "-c" ]]
 then
-    if [ $2 == "gnu" ]
+    if [[ $2 == "gnu" ]]
     then
         export CC=/usr/bin/gcc
         export CXX=/usr/bin/g++
 
         cmake ..
-    elif [ $2 == "clang" ]
+    elif [[ $2 == "clang" ]]
     then
-        export CC=/usr/bin/clang-5.0
-        export CXX=/usr/bin/clang++-5.0
+        export CC=/usr/bin/clang
+        export CXX=/usr/bin/clang++
 
         cmake ..
     fi
@@ -28,4 +29,6 @@ else
 	cmake ..
 fi
 
-make
+core_number=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
+jobs_number=$(awk "BEGIN {print 1.5 * $core_number}")
+make -j ${jobs_number}
